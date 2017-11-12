3a19e31b00f4840aaabb5f8ae7a4a731b16488de
Kaniar-Kode <noel.christophe.11@gmail.com>
2017-11-12 17:22:11 +0400

    Commit Color 1.0

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
-/**
- * Generated class for the NewsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,7 +10,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NewsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  songs: AngularFireList<any>;
+
+  constructor(public navCtrl: NavController, afDatabase: AngularFireDatabase) {
+    
   }
 
   ionViewDidLoad() {
